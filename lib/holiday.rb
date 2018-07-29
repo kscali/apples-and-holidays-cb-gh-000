9eba8cff25e02833126be6fa004443939ff18967
Koy Saeteurn <kosatun@aol.com>
2018-07-29 02:25:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
   # {
   #   :winter => {
   #     :christmas => ["Lights", "Wreath"],
@@ -17,35 +16,53 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |season, holiday|
+        holiday.each do |day, supplies|
+          if day == :christmas || day == :new_years
+            supplies << supply
+        end  
+      end 
+    end 
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+  holiday_hash.each do |season, holiday|
+        holiday.each do |day, supplies|
+          if day == :memorial_day
+            supplies << supply
+        end  
+      end 
+    end 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_hash.each do |season_s, holiday|
+    holiday_hash[season_s][holiday_name] = supply_array
+  end
 end
+
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].map do |holiday, supplies|
+    supplies
+  end.flatten
 end
 
+
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+     puts "#{season.to_s.capitalize}:"
+     holiday.each do |day, supplies|
+       puts "#{day.to_s.capitalize}: #{day.to_s.values.capitalize}"
+      end 
+  end 
+ 
+       
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
